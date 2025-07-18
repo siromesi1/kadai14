3e608ecb9d4c33a4acc53a40d6c6f3f1360b9568
siromesi1 <2401202@s.asojuku.ac.jp>
2025-07-18 10:35:32 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,14 +4,14 @@ $(function() {
     $('#btn').on('click', function(){
         $.ajax({
             //リクエスト先のURLを設定
-
+            url: "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $('#zipcode').val(),
             //レスポンスのデータ形式を設定
-
+            dataType : 'jsonp',
         }).done(function(data){
             //通信が成功したときの処理
             if(data.results){
                 //データが取得できたとき、setData関数を呼び出す
-
+                setData(data.results[0]);
             }else{
                 alert("該当するデータが見つかりませんでした");
             }
@@ -24,5 +24,7 @@ $(function() {
 
 function setData(data){
     //取得データを各HTML要素に代入
-
+    $('#prefecture').val(data.address1);
+    $('#city').val(data.address2);
+    $('#address').val(data.address3);
 }
